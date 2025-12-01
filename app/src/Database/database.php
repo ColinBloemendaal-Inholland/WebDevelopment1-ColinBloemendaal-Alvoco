16d42de79c0302794d8aced9e17e5a2d926aa393
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-01 20:14:29 +0100

    Added database connection

---
+++ app/src/Database/database.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace App\Database;
+
+use App\Config;
+use PDO;
+
+/**
+ * Summary of Database.
+ * Lets you connect to the database.
+ */
+class Database
+{
+    public static function getConnection(): PDO
+    {
+        try {
+            // set connection string
+            $connectionString = 'mysql:host=' . Config::DB_SERVER_NAME . ';dbname=' . Config::DB_NAME . ';charset=utf8mb4';
+
+            // create PDO connection
+            $connection = new PDO(
+                $connectionString, 
+                Config::DB_SERVER_USER,
+                Config::DB_PASSWORD);
+
+            // set error mode to exception
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $connection;
+        } catch (\Exception $e) {
+            throw new \Exception('Database connection error: ' . $e->getMessage());
+        }
+    }
+}
