19b069e7ee595ff73929cd9217a88baba0db27be
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-01 20:14:46 +0100

    redid the app structure

---
+++ app/dbconfig.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace App;
+
+class Config {
+    public const DB_SERVER_NAME = "mysql";
+    public const DB_SERVER_USER = 'root';
+    public const DB_PASSWORD = 'p90R2Z,kFM7?';
+    public const DB_NAME = 'Alvoco';
+}
