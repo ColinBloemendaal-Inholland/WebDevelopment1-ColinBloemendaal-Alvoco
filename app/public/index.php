7a2a0b51764dfe7713a617413f4e620131dfb905
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-05 12:46:36 +0100

    Made the eloquent ORM working

--- app/public/index.php
+++ app/public/index.php
@@ -1,13 +1,44 @@
 <?php
 use FastRoute\RouteCollector;
 use function FastRoute\simpleDispatcher;
+use App\Helpers\View;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use App\Config;
+
 require __DIR__ . '/../vendor/autoload.php';
 define('ROOT', value: dirname(__DIR__) . '/'); // points to /app
 
 $dispatcher = simpleDispatcher(function (RouteCollector $r) {
     $r->addRoute('GET', '/', ['App\Controllers\HomeController', 'index']);
-    $r->addRoute('GET', '/hello/{name}', ['App\Controllers\HelloController', 'greet']);
+    
+    $r->addRoute('GET','/leden', ['App\Controllers\LedenController','index']);
+
+    $r->addRoute('GET','/admin/leden', ['App\Controllers\AdminController','index']);
+    $r->addRoute('GET','/admin/leden/', ['App\Controllers\AdminController','index']);
+
+    $r->addRoute('GET','/api/leden', ['App\Controllers\LedenController','GetLeden']);
+
+    $r->addRoute('GET','/login', ['App\Controllers\LedenController','loginView']);
+    $r->addRoute('POST','/login', ['App\Controllers\LedenController','login']);
 });
+
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'mysql',
+    'database'  => Config::DB_NAME,
+    'username'  => Config::DB_SERVER_USER,
+    'password'  => Config::DB_PASSWORD,
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
 
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
@@ -17,7 +48,7 @@ $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
         http_response_code(404);
-        echo 'Not Found';
+        View::View("errors.404");
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         http_response_code(405);
