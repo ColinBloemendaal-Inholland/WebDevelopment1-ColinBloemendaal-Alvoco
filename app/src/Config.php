aa339448ad18e798cc9d189e8cc200518d550585
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-05 12:46:05 +0100

    Changed location of the dbconfig file. Not inside the App namespace

--- app/src/Config.php
+++ app/src/Config.php
@@ -5,6 +5,6 @@ namespace App;
 class Config {
     public const DB_SERVER_NAME = "mysql";
     public const DB_SERVER_USER = 'root';
-    public const DB_PASSWORD = 'p90R2Z,kFM7?';
+    public const DB_PASSWORD = 'secret123';
     public const DB_NAME = 'Alvoco';
 }
