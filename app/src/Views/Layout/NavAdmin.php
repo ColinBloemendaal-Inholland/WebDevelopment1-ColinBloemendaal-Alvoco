c3918fa8c1a00f96b252e5d68ba754572a48ed58
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-06 18:34:52 +0100

    Added admin nav

---
+++ app/src/Views/Layout/NavAdmin.php
@@ -0,0 +1,78 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" href="/admin">
+                    <i class="bi bi-house-door-fill"></i> Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a class="nav-link active" href="/admin/nieuwsberichten">
+                        <i class="bi bi-newspaper"></i> Nieuwsberichten
+                    </a>
+                    <a class="nav-link" href="/admin/nieuwsberichten/create">
+                        <i class="bi bi-plus-circle"></i>
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a class="nav-link active" href="/admin/teams">
+                        <i class="bi bi-people-fill"></i> Teams
+                    </a>
+                    <a class="nav-link" href="/admin/teams/create">
+                        <i class="bi bi-plus-circle"></i>
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a class="nav-link active" href="/admin/leden">
+                        <i class="bi bi-person-fill"></i> Leden
+                    </a>
+                    <a class="nav-link" href="/admin/leden/create">
+                        <i class="bi bi-plus-circle"></i>
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a class="nav-link active" href="/admin/coaches">
+                        <i class="bi bi-person-fill"></i> Coaches
+                    </a>
+                    <a class="nav-link" href="/admin/coaches/create">
+                        <i class="bi bi-plus-circle"></i>
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a class="nav-link active" href="/admin/trainers">
+                        <i class="bi bi-person-fill"></i> Trainers
+                    </a>
+                    <a class="nav-link" href="/admin/trainers/create">
+                        <i class="bi bi-plus-circle"></i>
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a class="nav-link active" href="/admin/wedstrijden">
+                        <i class="bi bi-person-fill"></i> Wedstrijden
+                    </a>
+                    <a class="nav-link" href="/admin/wedstrijden/create">
+                        <i class="bi bi-plus-circle"></i>
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a class="nav-link active" href="/admin/wedstrijden">
+                        <i class="bi bi-person-fill"></i> Bestuurdleden
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
