aeb7877ef39beea6ea01c1750ce3e7c7d4979201
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-07 14:56:31 +0100

    Created nieuwsberichten model

---
+++ app/src/Models/nieuwsberichten.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Bestuursleden;
+
+
+class Nieuwsberichten extends Model {
+    use SoftDeletes;
+    protected $table = "Nieuwsberichten";
+
+    protected $fillable = [
+        "Title",
+        "Message",
+        "Bestuursleden_id",
+    ];
+
+    public function Authur(): BelongsTo
+    {
+        return $this->belongsTo(Bestuursleden::class);
+    }
+}
