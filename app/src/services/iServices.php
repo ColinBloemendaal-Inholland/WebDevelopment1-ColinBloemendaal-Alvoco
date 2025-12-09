f7a724c7307b38475d71bed1bb77a5e8a50366d2
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-09 20:49:41 +0100

    renamed methods and fixed paramethers

--- app/src/services/iServices.php
+++ app/src/services/iServices.php
@@ -7,9 +7,7 @@ interface IServices {
     public function getAll(): array;
     public function create(array $data): array;
     public function update(int $id, array $data): array;
-    public function delete(int $id): array;
-    public function destroy(int $id): array;
-    public function datatables(array $filters, int $start, int $length, int $draw): array;
-    public function format(array $data): array;
-
+    public function delete(int $id): bool;
+    public function destroy(int $id): bool;
+    public function filter(array $filters, ?int $start = null, ?int $limit = null): array;
 }
