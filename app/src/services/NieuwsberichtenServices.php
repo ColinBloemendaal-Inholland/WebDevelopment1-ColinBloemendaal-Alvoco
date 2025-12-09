c514e30887df19ec560b1322273a4494bed5f45b
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-09 21:30:57 +0100

    Implemented service interface in nieuwsberichtenservices

--- app/src/services/NieuwsberichtenServices.php
+++ app/src/services/NieuwsberichtenServices.php
@@ -5,7 +5,7 @@ namespace App\Services;
 use App\Models\Nieuwsberichten;
 use App\Repositories\NieuwsberichtenRepository;
 
-class NieuwsberichtenServices
+class NieuwsberichtenServices implements IServices
 {
     private NieuwsberichtenRepository $repository;
     public function __construct(?NieuwsberichtenRepository $nieuwsberichtenRepository = null)
@@ -13,9 +13,32 @@ class NieuwsberichtenServices
         $this->repository = $nieuwsberichtenRepository ?? new NieuwsberichtenRepository(new Nieuwsberichten());
     }
 
+    public function get(int $id): array {
+        return $this->repository->get($id)->toArray() ?? null;
+    }
+    public function getAll(): array {
+        return $this->repository->getAll()->toArray() ?? null;
+    }
+    public function create(array $data): array {
+        return $this->repository->create($data)->toArray() ?? null;
+    }
+    public function update(int $id, array $data, ?array $roles = null): array {
+        return $this->repository->update($id, $data, $roles)->toArray() ?? null;
+    }
+    public function delete(int $id): bool {
+        return $this->repository->delete($id) ?? false;
+    }
+    public function destroy(int $id): bool {
+        return $this->repository->destroy($id) ?? false;
+    }
+    public function filter(array $filters, ?int $start = null, ?int $limit = null): array {
+        //TODO: Make filter in the nieuwsberichten repo
+        return $this->repository->filter($filters, $start, $limit);
+    }
+
     public function getNieuwsberichtenForDatatable(array $filters, int $start, int $length, int $draw)
     {
-        $result = $this->repository->getFilterdNieuwsberichten($filters, $start, $length);
+        $result = $this->filter($filters, $start, $length);
         $formattedResults = $result['data']->map(function ($row) { return $this->format($row);})->toArray();
         return [
             "draw" => $draw,
