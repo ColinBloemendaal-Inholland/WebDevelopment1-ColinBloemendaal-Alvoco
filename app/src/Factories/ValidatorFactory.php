65e65f264f79e2ef98e5f48bb3c1361a0245a1cb
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-10 10:45:14 +0100

    Create validation handlers

---
+++ app/src/Factories/ValidatorFactory.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App\Factories;
+
+use Rakit\Validation\Validator;
+
+class ValidatorFactory
+{
+    public static function make()
+    {
+        return new Validator();
+    }
+}
---
+++ app/src/Requests/BaseRequests.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Requests;
+
+use App\Factories\ValidatorFactory;
+
+abstract class BaseRequests
+{
+    protected array $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    abstract public function rules(): array;
+
+    public function validate(): array
+    {
+        $validator = ValidatorFactory::make();
+        $validation = $validator->validate($this->data, $this->rules());
+        if ($validation->fails()) {
+            throw new \Exception(json_encode($validation->errors()->all()));
+        }
+        return $validation->getValidData();
+    }
+}
