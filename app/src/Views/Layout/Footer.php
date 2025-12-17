86532e78587892d47b5f37f9ca749be997a000bb
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-17 12:24:04 +0100

    Fixed bug that the form_old wasnt unset at the end of the page

--- app/src/Views/Layout/Footer.php
+++ app/src/Views/Layout/Footer.php
@@ -1,4 +1,7 @@
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<?php
+    unset($_SESSION["errors"], $_SESSION["form_old"]);
+?>
 </body>
 </html>
