75b86729dd41d315adf56bc1be3c7b3dc8cd155a
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-19 19:19:12 +0100

    Added page titles

--- app/src/Views/Admin/Coaches/index.php
+++ app/src/Views/Admin/Coaches/index.php
@@ -2,6 +2,8 @@
     <?php \View::Partial('Layout.NavAdmin'); ?>
     <div class="flex-grow-1 p-4">
         <div class="container-fluid m-0 py-5">
+            <h1 class="mb-4">Bestuursleden</h1>
+
         </div>
     </div>
 </div>
