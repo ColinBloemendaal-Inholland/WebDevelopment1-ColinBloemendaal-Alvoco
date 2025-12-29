a0b73a4c9cc8005b7b79cd5af3beaa35110c71c2
ColinBloemendaal <colin.bloemendaal@gmail.com>
2025-12-29 20:15:16 +0100

    Updated admin nav

--- app/src/Views/Layout/NavAdmin.php
+++ app/src/Views/Layout/NavAdmin.php
@@ -8,6 +8,36 @@
             </li>
             <li class="nav-item">
                 <div class="d-flex justify-content-between align-items-center">
+                    <a class="nav-link active" href="/admin/leden">
+                        <i class="bi bi-people-fill"></i> Leden
+                    </a>
+                    <a class="nav-link" href="/admin/leden/create">
+                        <i class="bi bi-plus-circle"></i>
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a class="nav-link active" href="/admin/teams">
+                        <i class="bi bi-diagram-3-fill"></i> Teams
+                    </a>
+                    <a class="nav-link" href="/admin/teams/create">
+                        <i class="bi bi-plus-circle"></i>
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a class="nav-link active" href="/admin/wedstrijden">
+                        <i class="bi bi-calendar-event-fill"></i> Wedstrijden
+                    </a>
+                    <a class="nav-link" href="/admin/wedstrijden/create">
+                        <i class="bi bi-plus-circle"></i>
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="d-flex justify-content-between align-items-center">
                     <a class="nav-link active" href="/admin/nieuwsberichten">
                         <i class="bi bi-newspaper"></i> Nieuwsberichten
                     </a>
@@ -18,20 +48,20 @@
             </li>
             <li class="nav-item">
                 <div class="d-flex justify-content-between align-items-center">
-                    <a class="nav-link active" href="/admin/teams">
-                        <i class="bi bi-people-fill"></i> Teams
+                    <a class="nav-link active" href="/admin/coaches">
+                        <i class="bi bi-person-badge-fill"></i> Coaches
                     </a>
-                    <a class="nav-link" href="/admin/teams/create">
+                    <a class="nav-link" href="/admin/coaches/create">
                         <i class="bi bi-plus-circle"></i>
                     </a>
                 </div>
             </li>
             <li class="nav-item">
                 <div class="d-flex justify-content-between align-items-center">
-                    <a class="nav-link active" href="/admin/leden">
-                        <i class="bi bi-person-fill"></i> Leden
+                    <a class="nav-link active" href="/admin/bestuursleden">
+                        <i class="bi bi-person-fill-check"></i> Bestuursleden
                     </a>
-                    <a class="nav-link" href="/admin/leden/create">
+                    <a class="nav-link" href="/admin/bestuursleden/create">
                         <i class="bi bi-plus-circle"></i>
                     </a>
                 </div>
@@ -48,38 +78,11 @@
             </li>
             <li class="nav-item">
                 <div class="d-flex justify-content-between align-items-center">
-                    <a class="nav-link active" href="/admin/coaches">
-                        <i class="bi bi-person-fill"></i> Coaches
-                    </a>
-                    <a class="nav-link" href="/admin/coaches/create">
-                        <i class="bi bi-plus-circle"></i>
-                    </a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <div class="d-flex justify-content-between align-items-center">
                     <a class="nav-link active" href="/admin/trainers">
-                        <i class="bi bi-person-fill"></i> Trainers
+                        <i class="bi bi-person-vcard-fill"></i> Trainers
                     </a>
                     <a class="nav-link" href="/admin/trainers/create">
                         <i class="bi bi-plus-circle"></i>
-                    </a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <div class="d-flex justify-content-between align-items-center">
-                    <a class="nav-link active" href="/admin/wedstrijden">
-                        <i class="bi bi-person-fill"></i> Wedstrijden
-                    </a>
-                    <a class="nav-link" href="/admin/wedstrijden/create">
-                        <i class="bi bi-plus-circle"></i>
-                    </a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <div class="d-flex justify-content-between align-items-center">
-                    <a class="nav-link active" href="/admin/bestuursleden">
-                        <i class="bi bi-person-fill"></i> Bestuurdleden
                     </a>
                 </div>
             </li>
