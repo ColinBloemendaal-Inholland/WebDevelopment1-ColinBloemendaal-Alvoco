74db49a815384861bf3fbbcc4a90b567f1a0c429
ColinBloemendaal <colin.bloemendaal@gmail.com>
2026-01-08 19:06:21 +0100

    Added button to the nieuwsberichten in the user dashboard

--- app/src/Views/Dashboard/index.php
+++ app/src/Views/Dashboard/index.php
@@ -158,11 +158,15 @@
                         <h5 class="mt-3">Jouw 5 meest recente nieuwsberichten</h5>
                         <ul class="list-group mb-3">
                             <?php foreach ($data['recentNews'] as $news): ?>
-                                <li class="list-group-item">
-                                    <strong><?= e($news->Title) ?></strong><br>
-                                    <small class="text-muted">Geplaatst op
-                                        <?= e(date('d-m-Y H:i', strtotime($news->created_at))) ?></small>
-                                    <div><?= e($news->preview()) ?></div>
+                                <li class="list-group-item d-flex flex-md-row flex-column justify-content-between align-items-start gap-2">
+                                    <div class="flex-grow-1">
+                                        <div class="d-flex align-items-center">
+                                            <strong class="me-2 mb-0" style="line-height:1.5;"><?= e($news->Title) ?></strong>
+                                            <a href="/nieuwsberichten/<?= e($news->id) ?>" class="btn btn-primary btn-sm ms-auto">Bekijk</a>
+                                        </div>
+                                        <small class="text-muted">Geplaatst op <?= e(date('d-m-Y H:i', strtotime($news->created_at))) ?></small>
+                                        <div><?= e($news->preview()) ?></div>
+                                    </div>
                                 </li>
                             <?php endforeach; ?>
                         </ul>
