1195e4b0a444d1316ae6394cd43c7d84d1f12c9a
ColinBloemendaal <colin.bloemendaal@gmail.com>
2026-01-08 17:15:29 +0100

    Added 401 http view

---
+++ app/src/Views/Errors/401.php
@@ -0,0 +1,8 @@
+<div class="container d-flex justify-content-center align-items-center min-vh-100">
+  <div class="card shadow-lg p-4 text-center" style="max-width: 500px; width: 100%;">
+    <h1 class="display-1 fw-bold mb-2 text-info">401</h1>
+    <h2 class="h3 mb-3">Niet geautoriseerd</h2>
+    <p class="mb-4">Je bent niet ingelogd of hebt geen geldige autorisatie om deze pagina te bekijken.<br>Log in of neem contact op met de beheerder als je denkt dat dit een fout is.</p>
+    <a href="/" class="btn btn-primary">Terug naar Home</a>
+  </div>
+</div>
