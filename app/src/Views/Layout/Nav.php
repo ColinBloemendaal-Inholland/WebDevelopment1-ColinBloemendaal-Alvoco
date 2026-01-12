0f9d5cd1561f0decec0dd4457151ab8b4b4e8cc6
ColinBloemendaal <colin.bloemendaal@gmail.com>
2026-01-12 09:44:39 +0100

    Added profile to nav

--- app/src/Views/Layout/Nav.php
+++ app/src/Views/Layout/Nav.php
@@ -29,20 +29,31 @@
                 <?php if (\Auth::isLoggedIn()): ?>
                     <!-- User is logged in -->
                     <li class="nav-item dropdown">
-                        <button type="button" class="nav-link dropdown-toggle" id="userDropdown" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            <i class="bi bi-person-circle"></i> <?= htmlspecialchars(\Auth::email()); ?>
+                        <button type="button" class="nav-link dropdown-toggle" id="userDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-person-circle"></i> <?= e(\Auth::email()); ?>
                         </button>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-                            <li><a class="dropdown-item" href="/admin">
-                                    <i class="bi bi-speedometer2"></i> Dashboard
-                                </a></li>
+                            <li>
+                                <a class="dropdown-item" href="/admin">
+                                    <i class="bi bi-person-fill"></i> Profile
+                                </a>
+                            </li>
+                            <?php if(\Auth::user()->hasAnyRole(['beheerder', 'bestuurslid']) ): ?>
+                            <li>
+                                <a class="dropdown-item" href="/admin">
+                                    <i class="bi bi-speedometer2"></i> Admin
+                                </a>
+                            </li>
+                            <?php endif; ?>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
-                            <li><a class="dropdown-item text-danger" href="/logout">
+                            <li>
+                                <a class="dropdown-item text-danger" href="/logout">
                                     <i class="bi bi-box-arrow-right"></i> Uitloggen
-                                </a></li>
+                                </a>
+                            </li>
                         </ul>
                     </li>
                 <?php else: ?>
