474b080771b62bd1ee18bd0576942f817960fdfe
ColinBloemendaal <colin.bloemendaal@gmail.com>
2026-01-12 10:44:44 +0100

    Added integrety check

--- app/src/Views/Layout/Head.php
+++ app/src/Views/Layout/Head.php
@@ -11,14 +11,18 @@
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
     <!-- DataTables -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <!-- Tom Select -->
     <script src="https://cdn.jsdelivr.net/npm/tom-select/dist/js/tom-select.complete.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/tom-select/dist/css/tom-select.bootstrap5.min.css" rel="stylesheet">
-    <script src="https://cdn.tiny.cloud/1/64qg5v2oulxlnyjhnn6x5p21xuqa5nen9usdr8sbvddpufv4/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+    <script src="https://cdn.tiny.cloud/1/64qg5v2oulxlnyjhnn6x5p21xuqa5nen9usdr8sbvddpufv4/tinymce/8/tinymce.min.js"
+        crossorigin="anonymous" referrerpolicy="origin">
+        </script>
 
     <style>
         /* Ensure dropdown menus appear above sidebar */
