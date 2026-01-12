77acc67fedd9bd633836f321362e93afab5b1100
ColinBloemendaal <colin.bloemendaal@gmail.com>
2026-01-12 21:26:28 +0100

    removed already finished todo

--- app/src/services/BestuursledenServices.php
+++ app/src/services/BestuursledenServices.php
@@ -33,7 +33,6 @@ class BestuursledenServices implements IServices {
         return $this->repository->destroy($id) ?? false;
     }
     public function filter(array $filters, ?int $start = null, ?int $limit = null): array {
-        //TODO: Make filter in the bestuursleden repo
         return $this->repository->filter($filters, $start, $limit);
     }
     public function datatable(array $filters, int $start, $length, int $draw): array {
