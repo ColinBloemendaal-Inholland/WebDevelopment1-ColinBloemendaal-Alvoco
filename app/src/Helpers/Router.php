66b12099767af7f519cdd87133ff4986e75608ec
COAR_ <colin.bloemendaal@gmail.com>
2026-01-16 22:22:18 +0100

    Enabled all the exceptions again

--- app/src/Helpers/Router.php
+++ app/src/Helpers/Router.php
@@ -82,11 +82,10 @@ class Router
             http_response_code(404);
             \View::view("errors.404", '404');
         }
-        //TODO: TURN THIS BACK ON AFTER DEBUGGING 
-        // catch (Throwable $e) {
-        //     http_response_code(500);
-        //     \View::view("errors.500", '500');
-        // }
+        catch (Throwable $e) {
+            http_response_code(500);
+            \View::view("errors.500", '500');
+        }
     }
 
 }
